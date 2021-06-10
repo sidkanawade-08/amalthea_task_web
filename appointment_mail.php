0d641090496089684369d5e3c3acec9fb16eea49
sidkanawade-08 <76491610+sidkanawade-08@users.noreply.github.com>
2021-06-10 13:40:51 +0530

    amalthea web dev assignment

---
+++ appointment_mail.php
@@ -0,0 +1,15 @@
+<?php
+//get data from form  
+
+
+$to = $_POST['Email'];
+$subject = "Confirmation Email";
+$txt ="Thankyou for Registering, you will be updated soon" \r\n "Take care";
+$headers = "From: siddhesh.kanawade@iitgn.ac.in" . "\r\n" .
+"CC: siddheshkanawade609@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
